2ccccf5b5dc3bd372d9a81b0d3731b22693caa40
thrizzle <thrizzle@terabyteis.me>
2022-03-16 19:08:01 -0400

    🐛

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 env = load_dotenv()
+from os import environ
 
 guild_id = 758827653842075709
 color = 0x00D0FF
@@ -7,4 +8,4 @@ category_id = 953401594500763648
 log_channel_id = 953401632148832347
 support_role_id = 893633129418461225
 help_channel = 875062444140154891
-token = env["TOKEN"] or ""
+token = environ["TOKEN"] or ""
