25644dc634448dd7e6d21cbeb485c3c92b0726e1
thrizzle <thrizzle@terabyteis.me>
2022-03-16 19:07:20 -0400

    🔒️

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
+env = load_dotenv()
+
 guild_id = 758827653842075709
 color = 0x00D0FF
 category_id = 953401594500763648
 log_channel_id = 953401632148832347
 support_role_id = 893633129418461225
 help_channel = 875062444140154891
+token = env["TOKEN"] or ""
